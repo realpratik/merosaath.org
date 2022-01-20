16e8fbbcae686e40e2bde3ad5743569103c55830
Pratik <danpheaus@gmail.com>
2022-01-21 08:27:53 +1000

    first upload uploaded

---
+++ buyimages.php
@@ -0,0 +1,18 @@
+<?php include "inc/header.php";
+       
+?>
+<section>
+<!--Image slide upwards -->
+    <h1>Promoting tourism in Nepal</h1>
+
+
+
+
+
+
+       
+    <!--    <iframe width="340" height="260" src="../videos/merosaath.m4v" frameborder="2" allowfullscreen></iframe>  --> 
+
+</section>
+
+<?php  include "inc/footer.php"; ?>
